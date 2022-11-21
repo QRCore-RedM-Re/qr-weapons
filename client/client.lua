85ca87d1903735e8fecb55f366f23293809e963b
R S K <66393045+mn9-29@users.noreply.github.com>
2022-11-21 13:10:50 +0300

    UPDATE THX TO RexShack#3041

--- client/client.lua
+++ client/client.lua
@@ -1,50 +1,85 @@
 local QRCore = exports['qr-core']:GetCoreObject()
-local CurrentWeaponData = {}
 local currentSerial = nil
+local UsedWeapons = {}
 
 RegisterNetEvent('qr-weapons:client:UseWeapon', function(weaponData, shootbool)
     local ped = PlayerPedId()
     local weaponName = tostring(weaponData.name)
-    local weaponHash = GetHashKey(weaponData.name)
-    local weaponSerial = tostring(weaponData.info.serie)
-	local weapon = Citizen.InvokeNative(0x8425C5F057012DAB, ped)
-    local ammo = tonumber(weaponData.info.ammo) or 0
-	local total = Citizen.InvokeNative(0x39D22031557946C1, ped, 0x38E6F55F, Citizen.ResultAsInteger()) -- ammo arrow 0x38E6F55F
-    if weaponName == 'weapon_bow' or weaponName == 'weapon_bow_improved' then
-        GiveWeaponToPed_2(PlayerPedId(), weaponHash, 0, false, true, 0, false, 0.5, 1.0, 752097756, false, 0.0, false)
-		if total == 0 then 
-			GiveWeaponToPed_2(PlayerPedId(), weaponHash, 0, false, true, 0, false, 0.5, 1.0, 752097756, false, 0.0, false)
-        	Citizen.InvokeNative(0x106A811C6D3035F3, PlayerPedId(), GetHashKey('AMMO_ARROW'), ammo+1, 752097756)
-		end
-    else
-        if Citizen.InvokeNative(0x8425C5F057012DAB, ped) ~= GetHashKey("WEAPON_UNARMED") then
-            SetCurrentPedWeapon(ped, `WEAPON_UNARMED`, true)
-            local getammo = GetAmmoInPedWeapon(ped, weaponHash)
-            local getammoclip = GetAmmoInClip(ped, weaponHash)
-            TriggerServerEvent('qr-weapons:server:SaveAmmo', weaponSerial, getammo, getammoclip)
-            currentSerial = weaponSerial
-        else 
-            Citizen.InvokeNative(0xB282DC6EBD803C75, ped, weaponHash, ammo, true, 0)
+    local hash = GetHashKey(weaponData.name)
+    if not UsedWeapons[tonumber(hash)] then
+        UsedWeapons[tonumber(hash)] = {
+            name = weaponData.name,
+            WeaponHash = hash,
+            data = weaponData,
+            serie = weaponData.info.serie,
+        }
+        if weaponName == 'weapon_bow' or weaponName == 'weapon_bow_improved' then
+            if weaponData.info.ammo == nil then
+				local hasItem = QRCore.Functions.HasItem('ammo_arrow', 1)
+                if hasItem then
+                    weaponData.info.ammo = Config.AmountArrowAmmo
+                    weaponData.info.ammoclip = Config.AmountArrowAmmo
+					TriggerServerEvent('qr-weapons:server:removeWeaponAmmoItem', 'ammo_arrow')
+                else
+                    weaponData.info.ammo = 0
+                    weaponData.info.ammoclip = 0
+					QRCore.Functions.Notify('no arrows in your inventory to load', 'error')
+                end
+            elseif weaponData.info.ammo == 0 then
+				local hasItem = QRCore.Functions.HasItem('ammo_arrow', 1)
+                if hasItem then
+                    weaponData.info.ammo = Config.AmountArrowAmmo
+                    weaponData.info.ammoclip = Config.AmountArrowAmmo
+                    TriggerServerEvent('qr-weapons:server:removeWeaponAmmoItem', 'ammo_arrow')
+                else
+                    weaponData.info.ammo = 0
+                    weaponData.info.ammoclip = 0
+					QRCore.Functions.Notify('no arrows in your inventory to load', 'error')
+                end
+            end
+            Citizen.InvokeNative(0x5E3BDDBCB83F3D84, ped, hash, 0, false, true)
+        else
+            if weaponData.info.ammo == nil then
+                weaponData.info.ammo = 0
+                weaponData.info.ammoclip = 0
+            end
+            Citizen.InvokeNative(0x5E3BDDBCB83F3D84, ped, hash, 0, false, true)
         end
+        if weaponName == 'weapon_bow' or weaponName == 'weapon_bow_improved' then
+            SetPedAmmo(ped, hash, weaponData.info.ammo)
+        else
+            SetPedAmmo(ped, hash, weaponData.info.ammo - weaponData.info.ammoclip)
+            SetAmmoInClip(ped, hash, weaponData.info.ammoclip)
+        end
+        SetCurrentPedWeapon(ped,hash,true)
+    else
+        local ammo = GetAmmoInPedWeapon(PlayerPedId(), hash)
+        local ammobool, ammoclip = GetAmmoInClip(PlayerPedId(),hash)
+		TriggerServerEvent('qr-weapons:server:SaveAmmo', weaponData.info.serie, ammo, ammoclip)
+        RemoveWeaponFromPed(ped,hash)
+        UsedWeapons[tonumber(hash)] = nil
     end
+	currentSerial = weaponData.info.serie
 end)
 
 -- load ammo
-RegisterNetEvent('qr-weapons:client:AddAmmo', function(type, amount, itemData)
+RegisterNetEvent('qr-weapons:client:AddAmmo', function(ammotype, amount, ammo)
     local ped = PlayerPedId()
     local weapon = Citizen.InvokeNative(0x8425C5F057012DAB, ped)
 	local weapongroup = GetWeapontypeGroup(weapon)
+	local currentSerial = currentSerial
 	if Config.Debug == true then
 		print(weapon)
 		print(weapongroup)
+		print(currentSerial)
 	end
 	if currentSerial ~= nil then
 		if weapongroup == -1101297303 then -- revolver weapon group
 			local total = Citizen.InvokeNative(0x015A522136D7F951, PlayerPedId(), weapon, Citizen.ResultAsInteger())
 			if total + (amount/2) < Config.MaxRevolverAmmo then
 				if QRCore.Shared.Weapons[weapon] then
-					Citizen.InvokeNative(0x106A811C6D3035F3, ped, GetHashKey(type), amount, 0xCA3454E6)
-					TriggerServerEvent('weapons:server:removeWeaponAmmoItem', itemData)
+					Citizen.InvokeNative(0x106A811C6D3035F3, ped, GetHashKey(ammotype), amount, 0xCA3454E6)
+					TriggerServerEvent('qr-weapons:server:removeWeaponAmmoItem', 'ammo_revolver')
 				end
 			else
 				QRCore.Functions.Notify('Max Ammo Capacity', 'error')
@@ -53,8 +88,8 @@ RegisterNetEvent('qr-weapons:client:AddAmmo', function(type, amount, itemData)
 			local total = Citizen.InvokeNative(0x015A522136D7F951, PlayerPedId(), weapon, Citizen.ResultAsInteger())
 			if total + (amount/2) < Config.MaxPistolAmmo then
 				if QRCore.Shared.Weapons[weapon] then
-					Citizen.InvokeNative(0x106A811C6D3035F3, ped, GetHashKey(type), amount, 0xCA3454E6)
-					TriggerServerEvent('weapons:server:removeWeaponAmmoItem', itemData)
+					Citizen.InvokeNative(0x106A811C6D3035F3, ped, GetHashKey(ammotype), amount, 0xCA3454E6)
+					TriggerServerEvent('qr-weapons:server:removeWeaponAmmoItem', 'ammo_pistol')
 				end
 			else
 				QRCore.Functions.Notify('Max Ammo Capacity', 'error')
@@ -63,8 +98,8 @@ RegisterNetEvent('qr-weapons:client:AddAmmo', function(type, amount, itemData)
 			local total = Citizen.InvokeNative(0x015A522136D7F951, PlayerPedId(), weapon, Citizen.ResultAsInteger())
 			if total + (amount/2) < Config.MaxRepeaterAmmo then
 				if QRCore.Shared.Weapons[weapon] then
-					Citizen.InvokeNative(0x106A811C6D3035F3, ped, GetHashKey(type), amount, 0xCA3454E6)
-					TriggerServerEvent('weapons:server:removeWeaponAmmoItem', itemData)
+					Citizen.InvokeNative(0x106A811C6D3035F3, ped, GetHashKey(ammotype), amount, 0xCA3454E6)
+					TriggerServerEvent('qr-weapons:server:removeWeaponAmmoItem', 'ammo_repeater')
 				end
 			else
 				QRCore.Functions.Notify('Max Ammo Capacity', 'error')
@@ -73,8 +108,8 @@ RegisterNetEvent('qr-weapons:client:AddAmmo', function(type, amount, itemData)
 			local total = Citizen.InvokeNative(0x015A522136D7F951, PlayerPedId(), weapon, Citizen.ResultAsInteger())
 			if total + (amount/2) < Config.MaxRifleAmmo then
 				if QRCore.Shared.Weapons[weapon] then
-					Citizen.InvokeNative(0x106A811C6D3035F3, ped, GetHashKey(type), amount, 0xCA3454E6)
-					TriggerServerEvent('weapons:server:removeWeaponAmmoItem', itemData)
+					Citizen.InvokeNative(0x106A811C6D3035F3, ped, GetHashKey(ammotype), amount, 0xCA3454E6)
+					TriggerServerEvent('qr-weapons:server:removeWeaponAmmoItem', 'ammo_rifle')
 				end
 			else
 				QRCore.Functions.Notify('Max Ammo Capacity', 'error')
@@ -83,8 +118,8 @@ RegisterNetEvent('qr-weapons:client:AddAmmo', function(type, amount, itemData)
 			local total = Citizen.InvokeNative(0x015A522136D7F951, PlayerPedId(), weapon, Citizen.ResultAsInteger())
 			if total + (amount/2) < Config.MaxRifleAmmo then
 				if QRCore.Shared.Weapons[weapon] then
-					Citizen.InvokeNative(0x106A811C6D3035F3, ped, GetHashKey(type), amount, 0xCA3454E6)
-					TriggerServerEvent('weapons:server:removeWeaponAmmoItem', itemData)
+					Citizen.InvokeNative(0x106A811C6D3035F3, ped, GetHashKey(ammotype), amount, 0xCA3454E6)
+					TriggerServerEvent('qr-weapons:server:removeWeaponAmmoItem', 'ammo_rifle')
 				end
 			else
 				QRCore.Functions.Notify('Max Ammo Capacity', 'error')
@@ -93,21 +128,11 @@ RegisterNetEvent('qr-weapons:client:AddAmmo', function(type, amount, itemData)
 			local total = Citizen.InvokeNative(0x015A522136D7F951, PlayerPedId(), weapon, Citizen.ResultAsInteger())
 			if total + (amount/2) < Config.MaxShotgunAmmo then
 				if QRCore.Shared.Weapons[weapon] then
-					Citizen.InvokeNative(0x106A811C6D3035F3, ped, GetHashKey(type), amount, 0xCA3454E6)
-					TriggerServerEvent('weapons:server:removeWeaponAmmoItem', itemData)
+					Citizen.InvokeNative(0x106A811C6D3035F3, ped, GetHashKey(ammotype), amount, 0xCA3454E6)
+					TriggerServerEvent('qr-weapons:server:removeWeaponAmmoItem', 'ammo_shotgun')
 				end
 			else
 				QRCore.Functions.Notify('Max Ammo Capacity', 'error')
-			end
-		elseif weapongroup == -1241684019 then -- arrow weapon group
-			local total = Citizen.InvokeNative(0x015A522136D7F951, PlayerPedId(), weapon, Citizen.ResultAsInteger())
-			if total + (amount/2) < Config.MaxArrowAmmo then
-				if QRCore.Shared.Weapons[weapon] then
-					Citizen.InvokeNative(0x106A811C6D3035F3, ped, GetHashKey(type), amount, 0xCA3454E6)
-					TriggerServerEvent('weapons:server:removeWeaponAmmoItem', itemData)
-				end
-			else
-				QRCore.Functions.Notify('max ammo capacity!', 'error')
 			end
 		else
 			QRCore.Functions.Notify('wrong ammo for weapon!', 'error')
@@ -115,6 +140,18 @@ RegisterNetEvent('qr-weapons:client:AddAmmo', function(type, amount, itemData)
     else
 		QRCore.Functions.Notify('you are not holding a weapon!', 'error')
     end
+end)
+
+-- throwable weapons
+RegisterNetEvent('qr-weapons:client:usethrowable', function(weapon, ammoCount, attachPoint)
+	local ped = PlayerPedId()
+    local addReason = GetHashKey('ADD_REASON_DEFAULT')
+    local weaponHash = GetHashKey(weapon)
+    -- request weapon asset
+    Citizen.InvokeNative(0x72D4CB5DB927009C, weaponHash, 0, true)
+    Wait(1000)
+    -- give weapon to ped
+    Citizen.InvokeNative(0x5E3BDDBCB83F3D84, ped, weaponHash, ammoCount, true, false, attachPoint, true, 0.0, 0.0, addReason, true, 0.0, false)
 end)
 
 -- update ammo loop
@@ -126,7 +163,7 @@ CreateThread(function()
 		local getammo = GetAmmoInPedWeapon(ped, heldWeapon)
 		local getammoclip = GetAmmoInClip(ped, heldWeapon)
 		if currentSerial ~= nil and heldWeapon ~= -1569615261 then
-			TriggerServerEvent('qr-weapons:server:SaveAmmo', currentSerial, getammo, getammoclip)
+			TriggerServerEvent('qr-weapons:server:SaveAmmo', currentSerial, tonumber(getammo), tonumber(getammoclip))
 		end		
 	end
 end)
--- server/server.lua
+++ server/server.lua
@@ -2,31 +2,121 @@ local QRCore = exports['qr-core']:GetCoreObject()
 
 -- start of use ammo
 
-QRCore.Functions.CreateUseableItem("ammo_arrow", function(source, item)
-    TriggerClientEvent('qr-weapons:client:AddAmmo', source, 'AMMO_ARROW', Config.AmountArrowAmmo, item)
-end)
-
-QRCore.Functions.CreateUseableItem("ammo_revolver", function(source, item)
+QRCore.Functions.CreateUseableItem('ammo_revolver', function(source, item)
     TriggerClientEvent('qr-weapons:client:AddAmmo', source, 'AMMO_REVOLVER', Config.AmountRevolverAmmo, item)
 end)
 
-QRCore.Functions.CreateUseableItem("ammo_pistol", function(source, item)
+QRCore.Functions.CreateUseableItem('ammo_pistol', function(source, item)
     TriggerClientEvent('qr-weapons:client:AddAmmo', source, 'AMMO_PISTOL', Config.AmountPistolAmmo, item)
 end)
 
-QRCore.Functions.CreateUseableItem("ammo_repeater", function(source, item)
+QRCore.Functions.CreateUseableItem('ammo_repeater', function(source, item)
     TriggerClientEvent('qr-weapons:client:AddAmmo', source, 'AMMO_REPEATER', Config.AmountRepeaterAmmo, item)
 end)
 
-QRCore.Functions.CreateUseableItem("ammo_rifle", function(source, item)
+QRCore.Functions.CreateUseableItem('ammo_rifle', function(source, item)
     TriggerClientEvent('qr-weapons:client:AddAmmo', source, 'AMMO_RIFLE', Config.AmountRifleAmmo, item)
 end)
 
-QRCore.Functions.CreateUseableItem("ammo_shotgun", function(source, item)
+QRCore.Functions.CreateUseableItem('ammo_shotgun', function(source, item)
     TriggerClientEvent('qr-weapons:client:AddAmmo', source, 'AMMO_SHOTGUN', Config.AmountShotgunAmmo, item)
 end)
 
 -- end of use ammo
+
+-- use throwables
+
+-- dynamitestick
+QRCore.Functions.CreateUseableItem('dynamitestick', function(source, item)
+	local src = source
+    local Player = QRCore.Functions.GetPlayer(src)
+	if Player.Functions.RemoveItem(item.name, 1, item.slot) then
+        TriggerClientEvent('qr-weapons:client:usethrowable', src, 'weapon_thrown_dynamite', 1, 0)
+		TriggerClientEvent('inventory:client:ItemBox', src, QRCore.Shared.Items['dynamitestick'], 'remove')
+    end
+end)
+
+-- poisonbottle
+QRCore.Functions.CreateUseableItem('poisonbottle', function(source, item)
+	local src = source
+    local Player = QRCore.Functions.GetPlayer(src)
+	if Player.Functions.RemoveItem(item.name, 1, item.slot) then
+        TriggerClientEvent('qr-weapons:client:usethrowable', src, 'weapon_thrown_poisonbottle', 1, 0)
+		TriggerClientEvent('inventory:client:ItemBox', src, QRCore.Shared.Items['poisonbottle'], 'remove')
+    end
+end)
+
+-- molotov
+QRCore.Functions.CreateUseableItem('molotov', function(source, item)
+	local src = source
+    local Player = QRCore.Functions.GetPlayer(src)
+	if Player.Functions.RemoveItem(item.name, 1, item.slot) then
+        TriggerClientEvent('qr-weapons:client:usethrowable', src, 'weapon_thrown_molotov', 1, 0)
+		TriggerClientEvent('inventory:client:ItemBox', src, QRCore.Shared.Items['molotov'], 'remove')
+    end
+end)
+
+-- throwing_knives
+QRCore.Functions.CreateUseableItem('throwing_knives', function(source, item)
+	local src = source
+    local Player = QRCore.Functions.GetPlayer(src)
+	if Player.Functions.RemoveItem(item.name, 1, item.slot) then
+        TriggerClientEvent('qr-weapons:client:usethrowable', src, 'weapon_thrown_throwing_knives', 3, 0)
+		TriggerClientEvent('inventory:client:ItemBox', src, QRCore.Shared.Items['throwing_knives'], 'remove')
+    end
+end)
+
+-- tomahawk
+QRCore.Functions.CreateUseableItem('tomahawk', function(source, item)
+	local src = source
+    local Player = QRCore.Functions.GetPlayer(src)
+	if Player.Functions.RemoveItem(item.name, 1, item.slot) then
+        TriggerClientEvent('qr-weapons:client:usethrowable', src, 'weapon_thrown_tomahawk', 3, 0)
+		TriggerClientEvent('inventory:client:ItemBox', src, QRCore.Shared.Items['tomahawk'], 'remove')
+    end
+end)
+
+-- bolas
+QRCore.Functions.CreateUseableItem('bolas', function(source, item)
+	local src = source
+    local Player = QRCore.Functions.GetPlayer(src)
+	if Player.Functions.RemoveItem(item.name, 1, item.slot) then
+        TriggerClientEvent('qr-weapons:client:usethrowable', src, 'weapon_thrown_bolas', 3, 0)
+		TriggerClientEvent('inventory:client:ItemBox', src, QRCore.Shared.Items['bolas'], 'remove')
+    end
+end)
+
+-- bolas_hawkmoth
+QRCore.Functions.CreateUseableItem('bolas_hawkmoth', function(source, item)
+	local src = source
+    local Player = QRCore.Functions.GetPlayer(src)
+	if Player.Functions.RemoveItem(item.name, 1, item.slot) then
+        TriggerClientEvent('qr-weapons:client:usethrowable', src, 'weapon_thrown_bolas_hawkmoth', 3, 0)
+		TriggerClientEvent('inventory:client:ItemBox', src, QRCore.Shared.Items['bolas_hawkmoth'], 'remove')
+    end
+end)
+
+-- bolas_ironspiked
+QRCore.Functions.CreateUseableItem('bolas_ironspiked', function(source, item)
+	local src = source
+    local Player = QRCore.Functions.GetPlayer(src)
+	if Player.Functions.RemoveItem(item.name, 1, item.slot) then
+        TriggerClientEvent('qr-weapons:client:usethrowable', src, 'weapon_thrown_bolas_ironspiked', 3, 0)
+		TriggerClientEvent('inventory:client:ItemBox', src, QRCore.Shared.Items['bolas_ironspiked'], 'remove')
+    end
+end)
+
+-- bolas_intertwined
+QRCore.Functions.CreateUseableItem('bolas_intertwined', function(source, item)
+	local src = source
+    local Player = QRCore.Functions.GetPlayer(src)
+	if Player.Functions.RemoveItem(item.name, 1, item.slot) then
+        TriggerClientEvent('qr-weapons:client:usethrowable', src, 'weapon_thrown_bolas_intertwined', 3, 0)
+		TriggerClientEvent('inventory:client:ItemBox', src, QRCore.Shared.Items['bolas_intertwined'], 'remove')
+    end
+end)
+
+-- end of use throwables
 
 -- save ammo
 RegisterNetEvent('qr-weapons:server:SaveAmmo', function(serie, ammo, ammoclip)
@@ -35,8 +125,8 @@ RegisterNetEvent('qr-weapons:server:SaveAmmo', function(serie, ammo, ammoclip)
     local svslot = nil
     local itemData
     for v,k in pairs(Player.PlayerData.items) do
-		if k.type == "weapon" then
-			if ""..k.info.serie.."" == ""..serie.."" then
+		if k.type == 'weapon' then
+			if ''..k.info.serie..'' == ''..serie..'' then
 				svslot = k.slot
 				itemData = Player.Functions.GetItemBySlot(svslot)
 				itemData.info.ammo = ammo
@@ -49,11 +139,11 @@ RegisterNetEvent('qr-weapons:server:SaveAmmo', function(serie, ammo, ammoclip)
 end)
 
 -- remove ammo from player
-RegisterNetEvent('weapons:server:removeWeaponAmmoItem', function(item)
+RegisterServerEvent('qr-weapons:server:removeWeaponAmmoItem')
+AddEventHandler('qr-weapons:server:removeWeaponAmmoItem', function(ammoitem)
 	local src = source
     local Player = QRCore.Functions.GetPlayer(src)
-    if not Player or type(item) ~= 'table' or not item.name or not item.slot then return end
-    Player.Functions.RemoveItem(item.name, 1, item.slot)
-	TriggerClientEvent("inventory:client:ItemBox", src, QRCore.Shared.Items[item.name], "remove")
+	Player.Functions.RemoveItem(ammoitem, 1)
+	TriggerClientEvent('inventory:client:ItemBox', src, QRCore.Shared.Items[ammoitem], 'remove')
 	QRCore.Functions.Notify(src, 'Weapon Reloaded', 'success')
 end)
